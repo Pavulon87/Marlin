85c5c805c746135c5b470c54e95b5ffc7753e4f2
Pavulon87 <github@pavulon.net>
2020-09-01 16:39:35 +0200

    TMC current/HYBRID_THRESHOLD

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2229,7 +2229,7 @@
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
   #if AXIS_IS_TMC(X)
-  #define X_CURRENT       600        // (mA) RMS current. Multiply by 1.414 for peak current.
+  #define X_CURRENT       650        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16    // 0..256
     #define X_RSENSE          0.11
@@ -2245,7 +2245,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-  #define Y_CURRENT       500
+  #define Y_CURRENT       550
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
@@ -2261,7 +2261,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-  #define Z_CURRENT       600
+  #define Z_CURRENT       650
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
@@ -2293,7 +2293,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      650
+    #define E0_CURRENT      700
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
@@ -2469,22 +2469,22 @@
    */
   #define HYBRID_THRESHOLD
 
-  #define X_HYBRID_THRESHOLD     150  // [mm/s]
-  #define X2_HYBRID_THRESHOLD    150
-  #define Y_HYBRID_THRESHOLD     150
-  #define Y2_HYBRID_THRESHOLD    150
-  #define Z_HYBRID_THRESHOLD       3
-  #define Z2_HYBRID_THRESHOLD      3
-  #define Z3_HYBRID_THRESHOLD      3
-  #define Z4_HYBRID_THRESHOLD      3
-  #define E0_HYBRID_THRESHOLD     30
-  #define E1_HYBRID_THRESHOLD     30
-  #define E2_HYBRID_THRESHOLD     30
-  #define E3_HYBRID_THRESHOLD     30
-  #define E4_HYBRID_THRESHOLD     30
-  #define E5_HYBRID_THRESHOLD     30
-  #define E6_HYBRID_THRESHOLD     30
-  #define E7_HYBRID_THRESHOLD     30
+  #define X_HYBRID_THRESHOLD     175  // [mm/s]
+  #define X2_HYBRID_THRESHOLD    X_HYBRID_THRESHOLD
+  #define Y_HYBRID_THRESHOLD     175
+  #define Y2_HYBRID_THRESHOLD    Y_HYBRID_THRESHOLD
+  #define Z_HYBRID_THRESHOLD       5
+  #define Z2_HYBRID_THRESHOLD      Z_HYBRID_THRESHOLD
+  #define Z3_HYBRID_THRESHOLD      Z_HYBRID_THRESHOLD
+  #define Z4_HYBRID_THRESHOLD      Z_HYBRID_THRESHOLD
+  #define E0_HYBRID_THRESHOLD     50
+  #define E1_HYBRID_THRESHOLD     E0_HYBRID_THRESHOLD
+  #define E2_HYBRID_THRESHOLD     E0_HYBRID_THRESHOLD
+  #define E3_HYBRID_THRESHOLD     E0_HYBRID_THRESHOLD
+  #define E4_HYBRID_THRESHOLD     E0_HYBRID_THRESHOLD
+  #define E5_HYBRID_THRESHOLD     E0_HYBRID_THRESHOLD
+  #define E6_HYBRID_THRESHOLD     E0_HYBRID_THRESHOLD
+  #define E7_HYBRID_THRESHOLD     E0_HYBRID_THRESHOLD
 
   /**
    * Use StallGuard2 to home / probe X, Y, Z.
